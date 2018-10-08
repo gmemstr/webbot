108cd228aa7d83ca91d3d3ca7c37b095b0f5d3c4
Joel Rodriguez <joelerll@gmail.com>
2018-10-08 01:54:32 -0500

    remove jquery dependency, add feedgen dependency, add xml example

--- webassets/static/app.js
+++ webassets/static/app.js
@@ -1,32 +1,44 @@
-$(document).ready(function() {
-	//feed to parse
-	var feed = "/rss";
-
-	$.ajax(feed, {
-		accepts:{
-			xml:"application/rss+xml"
-		},
-		dataType:"xml",
-		success:function(data) {
-			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
-			$(data).find("item").each(function () { // or "item" or whatever suits your feed
-				var el = buildElement($(this))
-				$("#app").append(el);
-			});
-
-
-		}
-	});
-});
+(function() {
+   //feed to parse
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var parser = new DOMParser();
+	  var xmlDoc = parser.parseFromString(this.responseText,"text/xml");
+	  var items = xmlDoc.getElementsByTagName("item");
+	  for (var item of items) {
+	  	var el = buildElement(item);
+	  	var xmlString = el;
+  		var parser = new DOMParser();
+    	var doc = parser.parseFromString(xmlString, "text/html");
+    	var documento = doc.getElementsByTagName('body')[0].firstChild;
+	  	document.getElementById('app').appendChild(documento);
+	  }
+    }
+   };
+   xhttp.open("GET", "/rss", true);
+   xhttp.send();
+})();
 
 var buildElement = (el) => {
-    var element = "<div id='"+el.find("guid").text()+"' class='message'>";
-    element += "<h2>" + el.find("title").text() + "</h2>";
-    if (el.find("link").text() != "") {
-        element += "<a href='" + el.find("link").text() + "'>" + el.find("description").text() + "</a>";
+	var guid = el.getElementsByTagName("guid").item(0).textContent
+	var title = el.getElementsByTagName("title").item(0).textContent
+	var link = el.getElementsByTagName("link").item(0) ? el.getElementsByTagName("link").item(0).textContent : "" // sometimes dont sent link
+	var description = el.getElementsByTagName("description").item(0).textContent
+
+    var element = "<div id='" + guid + "' class='message'>";
+    element += "<h2>" + title + "</h2>";
+    if (link != "") {
+        element += "<a href='" + link + "'>" + description + "</a>";
     } else {
-        element += el.find("description").text();
+        element += description;
     }
     element += "</div>";
     return element;
+}
+
+function parseHTML(html) {
+    var t = document.createElement('template');
+    t.innerHTML = html;
+    return t.content.cloneNode(true);
 }
