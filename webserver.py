5a713b88ed2ab0797bef510672a3428399533a6c
JoseALermaIII <jose@JoseALerma.com>
2018-10-08 03:06:48 -0500

    Update webserver.py refactored new function: main

--- webserver.py
+++ webserver.py
@@ -1,21 +1,24 @@
 from flask import Flask, send_from_directory
 import config
 
-app = Flask(__name__,static_url_path='')
 
-@app.route('/')
-def serve_home():
-    return send_from_directory('webassets', 'index.html')
+def main():
+    app = Flask(__name__, static_url_path='')
+
+    @app.route('/')
+    def serve_home():
+        return send_from_directory('webassets', 'index.html')
+
+    @app.route('/static/<path:path>')
+    def send_js(path):
+        return send_from_directory('webassets/static', path)
+
+    @app.route('/rss')
+    def serve_rss_file():
+        return send_from_directory('.', 'discord.xml'), 200, {'Content-Type': 'application/rss+xml'}
+
+    app.run(port=config.flaskport, debug=config.flaskdebug)
 
 
-@app.route('/static/<path:path>')
-def send_js(path):
-    return send_from_directory('webassets/static', path)
-
-
-@app.route('/rss')
-def serve_rss_file():
-    return send_from_directory('.', 'discord.xml'), 200, {'Content-Type': 'application/rss+xml'}
-
-
-app.run(port=config.flaskport, debug=config.flaskdebug)
+if __name__ == '__main__':
+    main()
